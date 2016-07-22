221c1ddde7fe7e349bb6a5bd6f2677f53dc9bd6d
Tim Crockett <tim.crockett@avnet.com>
2016-07-22 17:43:13 -0600

    Tutorial Part II - Templates: Add template control statements to index.html Add render_template code to app.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Foo'}  # fake user
+    posts = [                   # fake array of posts
+        {
+            'author': {'nickname': 'Clown'},
+            'body': 'Funny how??  Funny ha ha?  Funny like a clown'
+        },
+        {
+            'author': {'nickname': 'Ethos'},
+            'body': {'For every bottle of me, Starbucks will donate 5 cents because that is all they can afford from selling $5 cups of coffee'}
+        }
+    ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
