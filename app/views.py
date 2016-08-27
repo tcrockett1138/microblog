2decdbf45dc04fd70d70ae5d37b436707772eb4c
Tim Crockett <tim.crockett@avnet.com>
2016-08-26 19:22:39 -0600

    Flask tutorial part VII - Unit testing/logging logging and duplicate user error resolved Add Sublime project files

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from .forms import LoginForm, EditForm
 from .models import User
 
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -21,9 +22,10 @@ def index():
         }
     ]
     return render_template('index.html',
-                        title='Home',
-                        user=user,
-                        posts=posts)
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
@@ -35,27 +37,28 @@ def login():
         session['remember_me'] = form.remember_me.data
         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
-                            title='Sign In',
-                            form=form,
-                            providers=app.config['OPENID_PROVIDERS'])
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @oid.after_login
 def after_login(resp):
-# No email associated with my Yahoo profile, so just add a dummy domain
     if resp.email is None or resp.email == "":
-        #flash('Invalid login.  Please try again.')
-        resp.email = resp.nickname + '@foo.com'
-#        return redirect(url_for('login'))
+        flash('Invalid login.  Please try again.')
+        return redirect(url_for('login'))
     user = User.query.filter_by(email=resp.email).first()
     if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
+        nickname = User.make_unique_nickname(nickname)
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
@@ -63,12 +66,14 @@ def after_login(resp):
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
-    login_user(user, remember = remember_me)
+    login_user(user, remember=remember_me)
     return redirect(request.args.get('next') or url_for('index'))
+
 
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 @app.before_request
 def before_request():
@@ -77,6 +82,7 @@ def before_request():
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
+
 
 @app.route('/usr/<nickname>')
 @login_required
@@ -90,13 +96,14 @@ def user(nickname):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html',
-                            user=user,
-                            posts=posts)
+                           user=user,
+                           posts=posts)
+
 
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-    form = EditForm()
+    form = EditForm(g.user.nickname)
     if form.validate_on_submit():
         g.user.nickname = form.nickname.data
         g.user.about_me = form.about_me.data
@@ -108,3 +115,14 @@ def edit():
         form.nickname.data = g.user.nickname
         form.about_me.data = g.user.about_me
         return render_template('edit.html', form=form)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
