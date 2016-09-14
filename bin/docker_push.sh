b6de1832400deeaf97ec34d0f24ce65062001844
Tim Crockett <tim.crockett@avnet.com>
2016-09-14 15:15:17 -0600

    Fixed variables so pushing to docker hub works correctly

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,26 +1,45 @@
-#! /bin/bash
+#! /bin/bash -xv
 
 #
+# Original:
 # http://dev.mikamai.com/post/144501132499/continuous-delivery-with-travis-and-ecs
-#
+# 
+# Modified to:
+#   Use Docker Hub instead of AWS ECR
+#   Add better error handling
 
 # Push only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master"]; then
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
   
     # This is needed to login on AWS and push the image on ECR
     # Change it accordingly to your docker repo
-    pip install --user awscli
+#    pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+#    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+    docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASS
     
     # Build and push
+    echo "Building $IMAGE_NAME"
     docker build -t $IMAGE_NAME .
+    retval=$?
+    if [ $retval -ne 0 ]; then
+      echo "Build failed"
+    else
+      echo "Build succeeded"
+    fi
+
     echo "Pushing $IMAGE_NAME:latest"
-    docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-    docker push "$REMOTE_IMAGE_URL:latest"
-    echo "Pushed $IMAGE_NAME:latest"
+    docker tag $IMAGE_NAME:latest "$REPOSITORY/$IMAGE_NAME:latest" && \
+    docker push "$REPOSITORY/$IMAGE_NAME:latest"
+    retval=$?
+    if [ $retval -ne 0 ]; then
+      echo "Tag/push failed"
+    else
+      echo "Pushed $IMAGE_NAME:latest"
+    fi
+    
   else
     echo "Skipping deploy because branch is not 'master'"
   fi
